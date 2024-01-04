abbb968d255021044ae1db678fd96d0e2b7d56fc
Amirhossein Shahraki <amirhossein.shahraki@protonmail.com>
2024-01-04 13:05:59 +0330

    🟢 Add support for configuring authentication service via environment variables

--- src/Authentication/Authentication.API/Program.cs
+++ src/Authentication/Authentication.API/Program.cs
@@ -1,17 +1,21 @@
 using Authentication.API.Utilities;
 using Authentication.Infrastructure;
+using DotNetEnv;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-builder.Services.Configure<RouteOptions>(options =>
+if (builder.Environment.IsDevelopment())
 {
-    options.LowercaseUrls = true;
-});
+    Env.Load();
+}
+
+// Add services to the container.
+builder.Configuration.AddEnvironmentVariables();
+builder.Services.AddControllers();
+builder.Services.Configure<RouteOptions>(options => { options.LowercaseUrls = true; });
 builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
 builder.Services.AddInfrastructureServices(builder.Configuration);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
